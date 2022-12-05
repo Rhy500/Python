81d5be5430aead2c3a258c804d2adaa07da14fec
Rahayu <febyanrahayu4000@gmail.com>
2022-12-05 12:40:41 +0900

    basic python

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
+print("basic math ")
+# this integer number
+int=[1,2,3,10,21,100,100]
+print(int)
+# this floating number
+flo=[1.5,2.33,10.55,0.2]
+print(flo)
+# this complex number
+com=[2j,10j,2.5j,0.21j]
+print(com)
 # Math questions
 
 x = 4
 y = 6
 sum = (x + y)
-print("sum") # results 10
+print(sum)
+
+y = 3
+h = 3
+print(y+h)
+ #addition
 
 x = 8
 y = 7
-print(x - y)
+print(x - y) 
+
+x = 8
+y = 7
+sum = (x-y)
+print(sum)
+#subraction
+
+g = 6
+h = 5
+print(g * h) #multiplication
+
+m = 100
+k = 2
+print(m/k) # Division
+
+m = 3**9
+print(m) # exponentiation
+
+h = 10 % 5
+print(h) # remaider ()same as 10:5 =quotient 2 ,remainder 0)
+
+h = 6//2
+print(h) # florr division
+
+j = 7-9*3
+print(j)
+
+j = (7-9)*3
+print(j)
+#operator sequence
 
 
 
 
 print("hello world!")
-# results of 2+3-4=
+
+ 
