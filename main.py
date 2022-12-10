9149e751c13386221e9af3a086de71d7286189ea
Rahayu <febyanrahayu4000@gmail.com>
2022-12-10 15:36:52 +0900

    add arithmetic and assignment

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 print("basic math ")
+
+#this python syntax in indentation
+if 9 > 4:
+ x = "this result"
+ print(x)
+
+if 9 < 4:
+    x = "this result"
+    print(x)
+
+if 4 < 9:
+    x = "this result"
+    print(x)
+
 # this integer number
 int=[1,2,3,10,21,100,100]
 print(int)
@@ -8,13 +22,17 @@ print(flo)
 # this complex number
 com=[2j,10j,2.5j,0.21j]
 print(com)
-# Math questions
+#this Booleans only has 2 vaule
+print(4>5)
+print(5==5)
+print(40<2.555)
 
+
+#python operator
 x = 4
 y = 6
 sum = (x + y)
 print(sum)
-
 y = 3
 h = 3
 print(y+h)
@@ -23,7 +41,6 @@ print(y+h)
 x = 8
 y = 7
 print(x - y) 
-
 x = 8
 y = 7
 sum = (x-y)
@@ -32,31 +49,48 @@ print(sum)
 
 g = 6
 h = 5
-print(g * h) #multiplication
+print(g * h)
+#multiplication
 
 m = 100
 k = 2
-print(m/k) # Division
+print(m/k) 
+# Division
 
 m = 3**9
-print(m) # exponentiation
+print(m)
+j=pow(6,2)
+print(j)
+ # exponentiation
 
 h = 10 % 5
-print(h) # remaider ()same as 10:5 =quotient 2 ,remainder 0)
+print(h)
+# remaider ()same as 10:5 =quotient 2 ,remainder 0)
 
 h = 6//2
-print(h) # florr division
+print(h) 
+# florr division
 
 j = 7-9*3
 print(j)
-
 j = (7-9)*3
 print(j)
 #operator sequence
 
 
+#python Assignment Operators
+x = "hello python"
+print(x)#Assignmen
+
+x = 4
+x += 3
+print(x) #Addition Assignment
+
+
 
 
 print("hello world!")
+
+
 
  
