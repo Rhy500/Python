80259465c22db98a7d4796992ebe96071db2008d
Rahayu <febyanrahayu4000@gmail.com>
2022-12-29 12:46:21 +0900

    add file data_type.py

---
+++ variabel.py
@@ -0,0 +1,25 @@
+# this variable 
+a = 10
+b = a + 3
+print(b)
+
+# menaruh / assigment value
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("nilai panjang = ", panjang)
+
+# name
+nilai_y = 15
+nilai10 = 1000000
+nilaiZ = 12.5
+
+print("Nilai a = ", a)
+a = 12
+print("Nilai a = ", a)
+
+
+ 
